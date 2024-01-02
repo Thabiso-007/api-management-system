1f79adae4fe8dd0955b67042e869c7e023cac831
Thabiso-007 <thabiso.hlatshwayo24@gmail.com>
2024-01-02 18:52:42 +0200

    first commit

---
+++ src/main/java/learning/apimanagementsystem/ApiManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package learning.apimanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiManagementSystemApplication.class, args);
+	}
+
+}
